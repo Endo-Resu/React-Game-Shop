1ed1bbb84764baca0279d8f9bd74eb5c4e6131a0
schiZis <stahanovgod@gmail.com>
2022-02-11 23:22:04 +0500

    added order page

--- src/components/cart-block/cart-block.js
+++ src/components/cart-block/cart-block.js
@@ -1,15 +1,23 @@
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import { useSelector } from "react-redux";
 import { BiCartAlt } from "react-icons/bi";
 import { CartMenu } from "../cart-menu";
 import { ItemsInCart } from "../items-in-cart";
 import { calcTotalPrice } from "../../utils/utils";
+import { useNavigate } from "react-router-dom";
 import './cart-block.css'
+
 
 export const CartBlock = () => {
     const [isCartMenuVisible, setCartMenuVisible] = useState(false);
     const items = useSelector(state => state.cart.itemsInCart);
     const totalPrice = calcTotalPrice(items);
+    const navigate = useNavigate();
+
+    const handleClick = useCallback(() => {
+        setCartMenuVisible(false);
+        navigate("/order");
+    }, [navigate]);
 
     return (
         <div className="cart-block">
@@ -20,7 +28,7 @@ export const CartBlock = () => {
                 onClick={() => setCartMenuVisible(!isCartMenuVisible)}
             />
             { totalPrice > 0 ? <span className="cart-block__total-price"> { totalPrice } руб.</span> : null}
-            {isCartMenuVisible && <CartMenu items={items} onClick={() => null }/>}
+            {isCartMenuVisible && <CartMenu items={items} onClick={handleClick}/>}
         </div>
     )
 }
--- src/components/cart-item/cart-item.js
+++ src/components/cart-item/cart-item.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { deleteItemFromCart } from "../../redux/cart/reducer";
+import { useDispatch } from "react-redux";
+import {AiOutlineCloseCircle} from "react-icons/ai";
 import './cart-item.css';
 
 export const CartItem = ({
@@ -6,11 +9,22 @@ export const CartItem = ({
     price,
     id
                          }) => {
+    const dispatch = useDispatch();
+
+    const handleClick = () => {
+        dispatch(deleteItemFromCart(id))
+    }
+
     return (
         <div className="cart-item">
             <span>{ title }</span>
             <div className="cart-item__price">
                 <span>{ price } руб.</span>
+                <AiOutlineCloseCircle
+                    size={25}
+                    className="cart-item__delete-icon"
+                    onClick={handleClick}
+                />
             </div>
         </div>
     )
